1ee8e21aababaad1b64daca335cc373aec50f857
soyocar <royomek@gmail.com>
2022-05-25 22:55:23 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 import "./styles.css";
+let btnEnv = document.getElementById("enviar");
+let indice: number;
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+btnEnv.addEventListener("click", () => {
+  let dimensionArreglo: number = Number(
+    prompt("Ingrese la dimensión del arreglo")
+  );
+  let arreglo: number[] = new Array(dimensionArreglo);
+  let resultado: number[] = new Array(dimensionArreglo);
+  function CargarArray(arraycarga: number[]) {
+    for (indice = 0; indice < dimensionArreglo; indice++) {
+      arraycarga[indice] = prompt(
+        `Ingrese el valor de la posicion ${indice}: `
+      );
+    }
+  }
+  function InvertiArray(Invertir: number[], resultado: number[]) {
+    let indice1: number = 0;
+    for (indice = dimensionArreglo - 1; indice <= 0; indice--) {
+      resultado[indice1] = Invertir[indice];
+      indice1 += 1;
+    }
+  }
+
+  CargarArray(arreglo);
+  InvertiArray(arreglo, resultado);
+  alert("El array invertido es " + resultado);
+});
